2e4c4b61e9634ad3a7a24843399fec91f6b9fdbc
Natalia <164662428+Natalia-Honcharova@users.noreply.github.com>
2026-05-03 15:03:52 +0300

    hw-8

---
+++ js/gallery.js
@@ -0,0 +1,57 @@
+const images = [
+  {
+    preview: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+    original: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview: "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original: "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview: "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original: "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+];
+
+const gallery = document.querySelector(".gallery");
+
+
+const markup = images
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const image = event.target.closest(".gallery-image");
+  if (!image) return;
+
+  const largeImage = image.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImage}" width="800" height="600">
+  `);
+
+  instance.show();
+}
